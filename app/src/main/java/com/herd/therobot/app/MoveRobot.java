9a3ecb1413d4c44252b6741354d6228a6aa032b7
Herd <ssherds@gmail.com>
2014-06-25 07:34:53 -0600

    Added comments

--- app/src/main/java/com/herd/therobot/app/MoveRobot.java
+++ app/src/main/java/com/herd/therobot/app/MoveRobot.java
@@ -23,15 +23,22 @@ import java.util.TimerTask;
 
 public class MoveRobot extends ActionBarActivity {
 
+    //used for keeping track of previous coordinates (we don't want to store duplicate data if the player is standing still)
     private double previousLongitude = 0.0;
     private double previousLatitude = 0.0;
 
+    //used for current coordinates
     double longitude = 0;
     double latitude = 0;
+
+    //keep the loop running
     boolean continueRunning;
     Timer timer;
 
+    //class which gives the users coordinates
     GPSTracker gps;
+
+    //determines if the following button has been pressed
     boolean following = false;
 
     // JSON parser class
@@ -43,28 +50,35 @@ public class MoveRobot extends ActionBarActivity {
     private static final String TAG_SUCCESS = "success";
     private static final String TAG_MESSAGE = "message";
 
+    //"run" method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.move_robot);
 
-        gps = new GPSTracker(MoveRobot.this);
-
+        //create links to buttons on page
         Button forward = (Button)findViewById(R.id.btnForward);
         Button left = (Button)findViewById(R.id.btnLeft);
         Button right = (Button)findViewById(R.id.btnRight);
         Button back = (Button)findViewById(R.id.btnBack);
         final Button followMe = (Button)findViewById(R.id.btnFollowMe);
 
+        //create listener for the forward button
         forward.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                //the user is pressing the forward button
+                //if currently following, we need to stop following and then continue with the manual move
                 if (following) {
+                    //stop following
                     followMe.performClick();
                 }
+                //turn on move forward
                 new Move("turnOn", "1").execute();
             } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                //the user released the forward button
+                //turn off move forward
                 new Move("turnOff", "1").execute();
             }
             return true;
@@ -75,11 +89,17 @@ public class MoveRobot extends ActionBarActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    //the user is pressing the left button
+                    //if currently following, we need to stop following and then continue with the manual move
                     if (following) {
+                        //stop following
                         followMe.performClick();
                     }
+                    //turn on move left
                     new Move("turnOn", "2").execute();
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    //the user released the left button
+                    //turn off move left
                     new Move("turnOff", "2").execute();
                 }
                 return true;
@@ -90,11 +110,17 @@ public class MoveRobot extends ActionBarActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    //the user is pressing the right button
+                    //if currently following, we need to stop following and then continue with the manual move
                     if (following) {
+                        //stop following
                         followMe.performClick();
                     }
+                    //turn on move right
                     new Move("turnOn", "3").execute();
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    //the user released the right button
+                    //turn off move right
                     new Move("turnOff", "3").execute();
                 }
                 return true;
@@ -105,11 +131,17 @@ public class MoveRobot extends ActionBarActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    //the user is pressing the back button
+                    //if currently following, we need to stop following and then continue with the manual move
                     if (following) {
+                        //stop following
                         followMe.performClick();
                     }
+                    //turn on move back
                     new Move("turnOn", "4").execute();
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    //the user released the back button
+                    //turn off move back
                     new Move("turnOff", "4").execute();
                 }
                 return true;
@@ -117,19 +149,24 @@ public class MoveRobot extends ActionBarActivity {
         });
 
         followMe.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View arg0) {
+                //if the user just clicked follow
                 if (!following) {
+                    //timer to determine how often to run the thread
                     timer = new Timer();
 
+                    //turn on following
                     following = true;
                     new Move("turnOn", "5").execute();
                     continueRunning = true;
+
+                    //change the button text
                     followMe.setText("Stop");
 
-                    //final Follow follow = new Follow(longitude, latitude);
                     final Handler handler = new Handler();
+
+                    //start new thread
                     TimerTask doAsynchronousTask = new TimerTask() {
                         @Override
                         public void run() {
@@ -139,8 +176,10 @@ public class MoveRobot extends ActionBarActivity {
                                         return; // stop when told to stop
                                     }
                                     try {
+                                        //create the new gps item
                                         gps = new GPSTracker(MoveRobot.this);
                                         if(gps.canGetLocation()){
+                                            //get the current coordinates
                                             longitude = gps.getLongitude();
                                             latitude = gps.getLatitude();
                                         } else {
@@ -149,8 +188,12 @@ public class MoveRobot extends ActionBarActivity {
 
                                         //TODO this part is causing data to only post once regardless of the user moving
                                         //This needs to be tested with the pi turned on
+
+                                        //compare current coordinates with previous coordinates
                                         if (previousLatitude != latitude || previousLongitude != longitude)
                                         {
+                                            //if in a new position,
+                                            //store current location and update previous location to current lovation
                                             previousLongitude = longitude;
                                             previousLatitude = latitude;
                                             Follow follow = new Follow(longitude, latitude);
@@ -161,30 +204,39 @@ public class MoveRobot extends ActionBarActivity {
                             });
                         }
                     };
-                    timer.schedule(doAsynchronousTask, 0, 5000); //execute in every 5 seconds
+                    timer.schedule(doAsynchronousTask, 0, 5000); //execute every 5 seconds
                 }
                 else {
+                    //user pressed stop or one of the manual control buttons performed the button stop button click
                     following = false;
+
+                    //change the button text
                     followMe.setText("Follow");
+
+                    //turn off follow
                     continueRunning = false;
                     new Move("turnOff", "5").execute();
+
+                    //terminate the thread
                     timer.cancel();
                 }
             }
         });
     }
 
+    //This class posts the gps coordinates
     class Follow extends AsyncTask<String, String, String> {
-
         private double longitude;
         private double latitude;
         private ProgressDialog pDialog;
 
+        //constructor
         public Follow(double longitudePassed, double latitudePassed) {
             longitude = longitudePassed;
             latitude = latitudePassed;
         }
 
+        //set up the progress dialog
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -195,6 +247,7 @@ public class MoveRobot extends ActionBarActivity {
             pDialog.show();
         }
 
+        //execute the post
         @Override
         protected String doInBackground(String... args) {
             // Check for success tag
@@ -230,6 +283,7 @@ public class MoveRobot extends ActionBarActivity {
             return null;
         }
 
+        //remove progress dialog
         protected void onPostExecute(String file_url) {
             // dismiss the dialog once product deleted
             pDialog.dismiss();
@@ -239,17 +293,19 @@ public class MoveRobot extends ActionBarActivity {
         }
     }
 
+    //this class posts the manual move action
     class Move extends AsyncTask<String, String, String> {
-
         private String action;
         private String pin;
         private ProgressDialog pDialog;
 
+        //constructor
         public Move(String actionPassed, String pinPassed) {
             action = actionPassed;
             pin = pinPassed;
         }
 
+        //display the progress dialog
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -260,6 +316,7 @@ public class MoveRobot extends ActionBarActivity {
             //pDialog.show();
         }
 
+        //execute move
         @Override
         protected String doInBackground(String... args) {
             // Check for success tag
@@ -294,6 +351,7 @@ public class MoveRobot extends ActionBarActivity {
             return null;
         }
 
+        //remove the progress dialog
         protected void onPostExecute(String file_url) {
             // dismiss the dialog once product deleted
             pDialog.dismiss();
